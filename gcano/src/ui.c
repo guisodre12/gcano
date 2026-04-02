d657886cdf38d09df64c3249fec88db17c9eca4f
Guilherme Sodré <92101150+guisodre12@users.noreply.github.com>
2026-04-02 04:32:20 +0000

    Refactor message input display logic in ui.c

--- gcano/src/ui.c
+++ gcano/src/ui.c
@@ -49,24 +49,24 @@ void start_ui(){
             case '\n':
                 if(highlight == 0){
                     // Mensagem aparece na tela
-    char mesg[]="Digite uma mensagem: ";
-    char str[80];
-
-    // para armazenar o número de linhas e o número de colunas da tela
-    int row,col;
-
-    // inicia a ncurses
-    initscr();
-
-    // obtém o número de linhas e colunas
-    getmaxyx(stdscr,row,col);
-
-    // imprime a mensagem no centro da tela
-    mvprintw(row/2,(col-strlen(mesg))/2,"%s",mesg);
-    getstr(str);
-    mvprintw(LINES - 2, 0, "Você digitou: %s", str);
-    getch();
-    endwin();
+                  char mesg[]="Digite uma mensagem: ";
+                  char str[80];
+              
+                  // para armazenar o número de linhas e o número de colunas da tela
+                  int row,col;
+              
+                  // inicia a ncurses
+                  initscr();
+              
+                  // obtém o número de linhas e colunas
+                  getmaxyx(stdscr,row,col);
+              
+                  // imprime a mensagem no centro da tela
+                  mvprintw(row/2,(col-strlen(mesg))/2,"%s",mesg);
+                  getstr(str);
+                  mvprintw(LINES - 2, 0, "Você digitou: %s", str);
+                  getch();
+                  endwin();
 
                 }
                 if(highlight == 2){ // Exit
